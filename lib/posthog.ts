2d09a970038001b6dc4fb18c41203665bd8e188e
Bryan Frimin <bryan@getprobo.com>
2025-05-17 11:20:06 -0700

    Do not enable posthog when not token set
    Signed-off-by: Bryan Frimin <bryan@getprobo.com>

--- lib/posthog.ts
+++ lib/posthog.ts
@@ -4,13 +4,15 @@ import posthog from "posthog-js";
 
 const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY!;
 
-if (typeof window !== "undefined") {
-  posthog.init(posthogKey, {
+if (posthogKey) {
+  if (typeof window !== "undefined") {
+    posthog.init(posthogKey, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     loaded: (posthog) => {
-      if (process.env.NODE_ENV === "development") posthog.debug();
-    },
-  });
+        if (process.env.NODE_ENV === "development") posthog.debug();
+      },
+    });
+  }
 }
 
 export { posthog };
