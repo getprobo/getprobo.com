8bf715adbdee8546721b224b62c8d3b90a32f239
Bryan Frimin <bryan@getprobo.com>
2025-05-18 12:51:55 -0700

    Fix footer link leading
    Signed-off-by: Bryan Frimin <bryan@getprobo.com>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -23,7 +23,7 @@ export function Footer() {
 
       <div className="grid gap-12 md:grid-cols-2 md:gap-0">
         <Image src="/probo-text.svg" alt="logo" width={191} height={80} />
-        <div className="text-dark-400 grid gap-6 text-base leading-normal font-normal whitespace-nowrap md:mr-30 md:grid-flow-col md:grid-cols-2 md:grid-rows-4 md:justify-self-end">
+        <div className="text-dark-400 grid gap-6 text-base leading-[100%] font-normal whitespace-nowrap md:mr-30 md:grid-flow-col md:grid-cols-2 md:grid-rows-4 md:justify-self-end">
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
           <Link href="/blog">Blog</Link>
