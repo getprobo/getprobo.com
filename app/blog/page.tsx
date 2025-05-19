d96446527ec827ca9a2f9d11440fe2636ebfc639
Bryan Frimin <bryan@getprobo.com>
2025-05-19 14:06:59 -0700

    Rewrite blog listing page
    Signed-off-by: Bryan Frimin <bryan@getprobo.com>

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,7 +2,7 @@ import { getAllPosts } from "@/lib/blog";
 import type { Metadata } from "next";
 import { ClientBlog } from "./ClientBlog";
 import siteConfig from "../metadata.config";
-import { Section } from "@/components/Section";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Blog",
@@ -59,31 +59,58 @@ export default function BlogPage() {
 
   return (
     <div>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
-      />
+      <section className="flex flex-col gap-4 py-10">
+        <h1 className="text-dark-900 text-center text-3xl leading-[120%] font-normal">
+          Blog
+        </h1>
+        <h2 className="text-dark-600 text-center text-sm leading-[150%] font-normal">
+          Need a specific article ? Tell us
+        </h2>
+      </section>
 
-      <Section>
-        <h1 className="mb-8 text-center text-6xl font-light">Blog</h1>
-      </Section>
-
-      <div className="bg-dark-900 mb-12 grid grid-cols-2 gap-6 rounded-2xl p-12 text-white lg:grid-cols-1">
-        {posts.slice(0, 2).map((post, index) => (
-          <ClientBlog key={post.slug} post={post} index={index} />
+      <section className="bg-dark-50 flex flex-col items-center gap-10 rounded-3xl py-10 md:gap-16 lg:flex-row lg:flex-wrap lg:justify-center">
+        {posts.map((post) => (
+          <article
+            key={post.slug}
+            className="flex w-80 flex-col gap-5 md:w-156"
+          >
+            <Image
+              src={post.coverImage}
+              alt={post.title}
+              width={100}
+              height={100}
+              className="h-auto w-full rounded-xl"
+            />
+            <div className="text-dark-900 flex flex-col gap-3">
+              <h2 className="text-lg leading-[140%] font-medium md:text-xl">
+                {post.title}
+              </h2>
+              <div className="flex flex-row items-center gap-2">
+                <Image
+                  src={post.author.image}
+                  alt={post.author.name}
+                  width={24}
+                  height={24}
+                  className="h-6 w-6 rounded-full md:h-8 md:w-8"
+                />
+                <p className="text-xs leading-[150%] font-medium md:text-base">
+                  {post.author.name}
+                </p>
+                <p className="text-xl leading-[150%] font-bold">•</p>
+                <p className="text-xs leading-[150%] md:text-base">
+                  {new Date(post.date).toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                    year: "numeric",
+                  })}
+                </p>
+              </div>
+            </div>
+          </article>
         ))}
-      </div>
-
-      {posts.slice(2).length > 0 && (
-        <div className="bg-dark-50 rounded-2xl p-12">
-          <h2 className="mb-8 text-4xl font-light">Latest Posts</h2>
-          <div className="mb-12 grid grid-cols-3 gap-6 lg:grid-cols-1">
-            {posts.slice(2).map((post, index) => (
-              <ClientBlog key={post.slug} post={post} index={index} />
-            ))}
-          </div>
-        </div>
-      )}
+        <div className="w-80 flex-col gap-5 sm:hidden md:w-156 lg:flex"></div>
+        <div className="w-80 flex-col gap-5 sm:hidden md:w-156 lg:flex"></div>
+      </section>
     </div>
   );
 }
