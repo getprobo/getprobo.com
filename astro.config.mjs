59d7813a750ab95de14b9e84346d87185b487413
Jonathan <contact@grafikart.fr>
2025-10-22 20:23:59 +0200

    Blog structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import preact from '@astrojs/preact';
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [preact(), mdx()]
 });
