2d51d07fc8f21f8cc42e291945f71266d462522b
Jonathan <contact@grafikart.fr>
2025-10-27 17:37:10 +0100

    Format

--- src/components/LogosScroll.svelte
+++ src/components/LogosScroll.svelte
@@ -1,50 +1,51 @@
 <script lang="ts">
-  import {onMount, type Snippet} from "svelte";
+  import { onMount, type Snippet } from "svelte";
   import clsx from "clsx";
   import Splide from "@splidejs/splide";
   import { AutoScroll } from "@splidejs/splide-extension-auto-scroll";
   import { Intersection } from "@splidejs/splide-extension-intersection";
 
-  const props: {children: Snippet, class?: string} = $props();
+  const props: { children: Snippet; class?: string } = $props();
   let innerWidth = $state(0);
   let slider: HTMLDivElement | null = null;
   const options = {
-      type: "loop",
-      gap: 16,
-      arrows: false,
-      autoWidth: true,
-      pagination: false,
-      mediaQuery: "min",
-        autoScroll: {
-          pauseOnFocus: false,
-          pauseOnHover: false,
-        }
+    type: "loop",
+    gap: 16,
+    arrows: false,
+    autoWidth: true,
+    pagination: false,
+    mediaQuery: "min",
+    autoScroll: {
+      pauseOnFocus: false,
+      pauseOnHover: false,
+    },
   } as const;
 
   onMount(() => {
     if (!slider) {
       return;
     }
-    const slot = slider.querySelector("astro-slot") as HTMLElement
-  slot.setAttribute("class", "splide__list items-center block");
-    Array.from(slot.children).forEach((child) => {child.classList.add("splide__slide")})
-    const s = new Splide(slider, options)
-      .mount({ AutoScroll, Intersection });
+    const slot = slider.querySelector("astro-slot") as HTMLElement;
+    slot.setAttribute("class", "splide__list items-center block");
+    Array.from(slot.children).forEach((child) => {
+      child.classList.add("splide__slide");
+    });
+    const s = new Splide(slider, options).mount({ AutoScroll, Intersection });
     return () => {
       s.destroy();
     };
   });
 </script>
 
-<div class={clsx(props.class, 'splide')}  bind:this={slider}>
-    <div class="splide__track">
-        {@render props.children()}
-    </div>
-    <div
-            class="absolute top-0 right-0 h-full w-60 bg-linear-to-l from-[#FFFFFB] to-[#FFFFFB]/0"
-    />
-    <div
-            class="absolute top-0 left-0 h-full w-60 bg-linear-to-r from-[#FFFFFB] to-[#FFFFFB]/0"
-    />
+<div class={clsx(props.class, "splide")} bind:this={slider}>
+  <div class="splide__track">
+    {@render props.children()}
+  </div>
+  <div
+    class="absolute top-0 right-0 h-full w-60 bg-linear-to-l from-[#FFFFFB] to-[#FFFFFB]/0"
+  />
+  <div
+    class="absolute top-0 left-0 h-full w-60 bg-linear-to-r from-[#FFFFFB] to-[#FFFFFB]/0"
+  />
 </div>
 <svelte:window bind:innerWidth />
