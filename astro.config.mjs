3a89e821e3f4e9a667a33f7f27ef68d589a2bcf6
Émile Ré <emile@probo.com>
2026-05-29 16:49:00 +0200

    style
    Signed-off-by: Émile Ré <emile@probo.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -117,10 +117,7 @@ export default defineConfig({
         },
       },
     },
-    plugins: [
-      svelteVirtualCssFallback(),
-      tailwindcss(),
-    ],
+    plugins: [svelteVirtualCssFallback(), tailwindcss()],
   },
 
   integrations: [
